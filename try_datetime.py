229788961e83425ed6b4613f5d564630ce4b614a
Chungsang Tom Lam <erinata@gmail.com>
2021-02-23 15:47:33 -0500

    initial commit

---
+++ try_datetime.py
@@ -0,0 +1,52 @@
+import pandas
+from datetime import datetime
+
+import numpy
+
+dataset = pandas.read_csv("chicago_crime_2018.csv")
+dataset = dataset.sample(frac=0.1).reset_index(drop=True)
+
+# print(dataset)
+
+# print(dataset['Date'])
+
+dataset['Date'] = pandas.to_datetime(dataset['Date'])
+
+# print(dataset['Date'])
+
+dataset['year'] = dataset['Date'].dt.year
+dataset['month'] = dataset['Date'].dt.month
+dataset['hour'] = dataset['Date'].dt.hour
+dataset['minute'] = dataset['Date'].dt.minute
+
+# print(dataset[['Date', 'year', 'month', 'hour']])
+
+
+dataset['hour_slot'] = numpy.select([
+	(dataset['hour'] < 4),
+	(dataset['hour'] < 8),
+	(dataset['hour'] < 12),
+	(dataset['hour'] < 16),
+	(dataset['hour'] < 20),
+	(dataset['hour'] < 24)]
+	, [0,1,2,3,4,5])
+
+
+# print(dataset[['Date', 'hour', 'hour_slot']])
+
+
+dataset['minute_slot'] = numpy.select([
+	(dataset['minute'] < 15),
+	(dataset['minute'] < 30),
+	(dataset['minute'] < 45),
+	(dataset['minute'] < 60)]
+	, [0,1,2,3])
+
+dataset['minutes_slot_in_day'] = dataset['hour']*4 + dataset['minute_slot'] 
+
+
+dataset['diff'] = dataset['Date'] - datetime(2018,1,24,0,0,0)
+
+# print(dataset[['Date', 'diff']])
+
+
